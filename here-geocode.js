e4e93e20005287dbb26603da4c8ea987fb29b8f3
Masa Hilal H Abushamleh <Masa.Abushamleh@ibm.com>
2020-09-15 15:57:36 +0400

    fixed the in parameter, let's test it

--- here-geocode.js
+++ here-geocode.js
@@ -21,19 +21,19 @@ module.exports = function(RED) {
         msg.hereparams.query = query; // take the default or the node setting
       } else {
         // passed in param, override default or node setting
-        msg.hereparams.query = msg.hereparams.query.toLowerCase();;
+        msg.hereparams.query = msg.hereparams.query.toLowerCase();
       }
 
       if( typeof msg.hereparams.in_var == 'undefined' ) {
         msg.hereparams.in_var = in_var; // take the default or the node setting
       } else {
         // passed in param, override default or node setting
-        msg.hereparams.in_var = msg.hereparams.in_var.toLowerCase();;
+        msg.hereparams.in_var = 'countryCode:'+msg.hereparams.in_var.toLowerCase();
       }
 
       (async () => {
         try {
-          const response = await axios.get('https://geocode.search.hereapi.com/v1/geocode?q='+msg.hereparams.query+'&apiKey='+apiKey);
+          const response = await axios.get('https://geocode.search.hereapi.com/v1/geocode?q='+msg.hereparams.query+'&in='+msg.hereparams.in_var+'&apiKey='+apiKey);
           //console.log(response.data)
           msg.payload = response.data;
           node.send(msg);
