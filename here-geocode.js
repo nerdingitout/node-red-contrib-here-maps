e4dd74d27da11becacd87343f5e8419d469d2390
Masa Hilal H Abushamleh <Masa.Abushamleh@ibm.com>
2020-09-23 14:58:28 +0400

    'in'

--- here-geocode.js
+++ here-geocode.js
@@ -33,7 +33,7 @@ module.exports = function(RED) {
 
       (async () => {
         try {
-          const response = await axios.get('https://geocode.search.hereapi.com/v1/geocode?q='+msg.hereparams.query+'&apiKey='+apiKey);
+          const response = await axios.get('https://geocode.search.hereapi.com/v1/geocode?q='+msg.hereparams.query+'&apiKey='+apiKey+'&in='+msg.hereparams.in_var);
           //console.log(response.data)
           msg.payload = response.data;
           node.send(msg);
