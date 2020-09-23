bc6c99d896a226c95e336667d10728c2d0b8bb4f
Masa Hilal H Abushamleh <Masa.Abushamleh@ibm.com>
2020-09-23 16:22:35 +0400

    limit parameter fixed

--- here-geocode.js
+++ here-geocode.js
@@ -35,7 +35,7 @@ module.exports = function(RED) {
         // passed in param, override default or node setting
         msg.hereparams.in_var = msg.hereparams.in_var;
       }
-
+      
       //limit, optional parameter
       if( typeof msg.hereparams.limit == 'undefined' ) {
         msg.hereparams.limit = limit; // take the default or the node setting
@@ -47,6 +47,9 @@ module.exports = function(RED) {
       //Add the optional parameters to the api call if they are not empty
       if(msg.hereparams.in_var!=""){
         api_str=api_str+'&in=countryCode:'+msg.hereparams.in_var;
+      }
+      if(msg.hereparams.limit!=0 || msg.hereparams.limit!=""){
+        api_str=api_str+'&limit='+msg.hereparams.limit;
       }
 
       (async () => {
